e702f89a08628c099ecbb131e86ed39919169133
g-prog <alaobukky2@gmail.com>
2021-09-15 04:04:40 -0700

    added some podcast functionality

--- index2.js
+++ index2.js
@@ -1,87 +1,178 @@
-(function(){
+const musicContainer = document.getElementById('music-container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
 
-    var pcastPlayers = document.querySelectorAll('.pcast-player');
-    var speeds = [ 1, 1.5, 2, 2.5, 3 ]    
-    
-    for(i=0;i<pcastPlayers.length;i++) {
-      var player = pcastPlayers[i];
-      var audio = player.querySelector('audio');
-      var play = player.querySelector('.pcast-play');
-      var pause = player.querySelector('.pcast-pause');
-      var rewind = player.querySelector('.pcast-rewind');
-      var progress = player.querySelector('.pcast-progress');
-      var speed = player.querySelector('.pcast-speed');
-      var mute = player.querySelector('.pcast-mute');
-      var currentTime = player.querySelector('.pcast-currenttime');
-      var duration = player.querySelector('.pcast-duration');
-      
-      var currentSpeedIdx = 0;
-  
-      pause.style.display = 'none';
-      
-      var toHHMMSS = function ( totalsecs ) {
-          var sec_num = parseInt(totalsecs, 10); // don't forget the second param
-          var hours   = Math.floor(sec_num / 3600);
-          var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-          var seconds = sec_num - (hours * 3600) - (minutes * 60);
-  
-          if (hours   < 10) {hours   = "0"+hours; }
-          if (minutes < 10) {minutes = "0"+minutes;}
-          if (seconds < 10) {seconds = "0"+seconds;}
-          
-          var time = hours+':'+minutes+':'+seconds;
-          return time;
-      }
-      
-      audio.addEventListener('loadedmetadata', function(){
-        progress.setAttribute('max', Math.floor(audio.duration));
-        duration.textContent  = toHHMMSS(audio.duration);
-      });
-      
-      audio.addEventListener('timeupdate', function(){
-        progress.setAttribute('value', audio.currentTime);
-        currentTime.textContent  = toHHMMSS(audio.currentTime);
-      });
-      
-      play.addEventListener('click', function(){
-        this.style.display = 'none';
-        pause.style.display = 'inline-block';
-        pause.focus();
-        audio.play();
-      }, false);
-  
-      pause.addEventListener('click', function(){
-        this.style.display = 'none';
-        play.style.display = 'inline-block';
-        play.focus();
-        audio.pause();
-      }, false);
-   
-      rewind.addEventListener('click', function(){
-        audio.currentTime -= 30;
-      }, false);
-      
-      progress.addEventListener('click', function(e){
-        audio.currentTime = Math.floor(audio.duration) * (e.offsetX / e.target.offsetWidth);
-      }, false);
-  
-      speed.addEventListener('click', function(){
-        currentSpeedIdx = currentSpeedIdx + 1 < speeds.length ? currentSpeedIdx + 1 : 0;
-        audio.playbackRate = speeds[currentSpeedIdx];
-        this.textContent  = speeds[currentSpeedIdx] + 'x';
-        return true;
-      }, false);
-  
-      mute.addEventListener('click', function() {
-        if(audio.muted) {
-          audio.muted = false;
-          this.querySelector('.fa').classList.remove('fa-volume-off');
-          this.querySelector('.fa').classList.add('fa-volume-up');
-        } else {
-          audio.muted = true;
-          this.querySelector('.fa').classList.remove('fa-volume-up');
-          this.querySelector('.fa').classList.add('fa-volume-off');
-        }
-      }, false);
-    }
-  })(this);
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+const currTime = document.querySelector('#currTime');
+const durTime = document.querySelector('#durTime');
+
+// Song titles
+const songs = ['hozier', 'avril'];
+
+// Keep track of song
+let songIndex = 0;
+
+// Initially load song details into DOM
+loadSong(songs[songIndex]);
+
+// Update song details
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/${song}.jpg`;
+}
+
+// Play song
+function playSong() {
+  musicContainer.classList.add('play');
+  playBtn.querySelector('i.fas').classList.remove('fa-play');
+  playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+  audio.play();
+}
+
+// Pause song
+function pauseSong() {
+  musicContainer.classList.remove('play');
+  playBtn.querySelector('i.fas').classList.add('fa-play');
+  playBtn.querySelector('i.fas').classList.remove('fa-pause');
+
+  audio.pause();
+}
+
+// Previous song
+function prevSong() {
+  songIndex--;
+
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+
+// Next song
+function nextSong() {
+  songIndex++;
+
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+
+// Update progress bar
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+// Set progress bar
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+
+  audio.currentTime = (clickX / width) * duration;
+}
+
+//get duration & currentTime for Time of song
+function DurTime (e) {
+	const {duration,currentTime} = e.srcElement;
+	var sec;
+	var sec_d;
+
+	// define minutes currentTime
+	let min = (currentTime==null)? 0:
+	 Math.floor(currentTime/60);
+	 min = min <10 ? '0'+min:min;
+
+	// define seconds currentTime
+	function get_sec (x) {
+		if(Math.floor(x) >= 60){
+			
+			for (var i = 1; i<=60; i++){
+				if(Math.floor(x)>=(60*i) && Math.floor(x)<(60*(i+1))) {
+					sec = Math.floor(x) - (60*i);
+					sec = sec <10 ? '0'+sec:sec;
+				}
+			}
+		}else{
+		 	sec = Math.floor(x);
+		 	sec = sec <10 ? '0'+sec:sec;
+		 }
+	} 
+
+	get_sec (currentTime,sec);
+
+	// change currentTime DOM
+	currTime.innerHTML = min +':'+ sec;
+
+	// define minutes duration
+	let min_d = (isNaN(duration) === true)? '0':
+		Math.floor(duration/60);
+	 min_d = min_d <10 ? '0'+min_d:min_d;
+
+
+	 function get_sec_d (x) {
+		if(Math.floor(x) >= 60){
+			
+			for (var i = 1; i<=60; i++){
+				if(Math.floor(x)>=(60*i) && Math.floor(x)<(60*(i+1))) {
+					sec_d = Math.floor(x) - (60*i);
+					sec_d = sec_d <10 ? '0'+sec_d:sec_d;
+				}
+			}
+		}else{
+		 	sec_d = (isNaN(duration) === true)? '0':
+		 	Math.floor(x);
+		 	sec_d = sec_d <10 ? '0'+sec_d:sec_d;
+		 }
+	} 
+
+	// define seconds duration
+	
+	get_sec_d (duration);
+
+	// change duration DOM
+	durTime.innerHTML = min_d +':'+ sec_d;
+		
+};
+
+// Event listeners
+playBtn.addEventListener('click', () => {
+  const isPlaying = musicContainer.classList.contains('play');
+
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
+
+// Change song
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+// Time/song update
+audio.addEventListener('timeupdate', updateProgress);
+
+// Click on progress bar
+progressContainer.addEventListener('click', setProgress);
+
+// Song ends
+audio.addEventListener('ended', nextSong);
+
+// Time of song
+audio.addEventListener('timeupdate',DurTime);
